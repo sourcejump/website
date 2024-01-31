f80d3e9ad546ecded588850e2b4634510dcee844
Worldspawn <worldspawn.web@gmail.com>
2024-01-31 20:19:47 +0300

    feat: MainPage

---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,9 @@
+import styles from './MainPage.module.scss';
+
+export const MainPage = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Project starts from here! :)</h1>
+    </div>
+  );
+};
