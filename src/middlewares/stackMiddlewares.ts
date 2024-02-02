846d67c00b116c234d27433c06f85463f209908a
Ruslan <157328003+0o22@users.noreply.github.com>
2024-02-02 04:01:11 +0200

    Setup chained middlewares with additional headers and route ignoring

---
+++ src/middlewares/stackMiddlewares.ts
@@ -0,0 +1,14 @@
+import { NextResponse, type NextMiddleware } from 'next/server';
+import type { MiddlewareFactory } from '@/middlewares/types';
+
+export function stackMiddlewares(functions: MiddlewareFactory[] = [], index = 0): NextMiddleware {
+  const current = functions[index];
+
+  if (current) {
+    const next = stackMiddlewares(functions, index + 1);
+
+    return current(next);
+  }
+
+  return () => NextResponse.next();
+}
