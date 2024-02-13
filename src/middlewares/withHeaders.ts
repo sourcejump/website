01a3fea8a085505036b6f1af0bb09f474b4eb920
Ruslan <157328003+0o22@users.noreply.github.com>
2024-02-13 17:40:35 +0200

    Add types and stack middlewares

---
+++ src/middlewares/withHeaders.ts
@@ -0,0 +1,18 @@
+import type { NextFetchEvent, NextMiddleware, NextRequest } from 'next/server';
+import type { MiddlewareFactory } from '@/middlewares/types';
+
+export const withHeaders: MiddlewareFactory = (next: NextMiddleware) => {
+  return async (request: NextRequest, _next: NextFetchEvent) => {
+    const response = await next(request, _next);
+
+    const { href, pathname } = request.nextUrl;
+
+    if (response) {
+      // Add headers because `request.nextUrl` is not available in the server components
+      response.headers.set('x-href', href);
+      response.headers.set('x-pathname', pathname);
+    }
+
+    return response;
+  };
+};
